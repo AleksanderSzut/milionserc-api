cd4d7b1753f18ecdce6a60a0c8df3adf763936e3
Aleksander Szut <aleksander.szut@paymento.pl>
2021-01-11 05:41:26 +0100

    - delete overwriting post data

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -11,21 +11,6 @@ class TransactionController extends Controller
 {
     public function create(Request $request)
     {
-        $_POST = array
-        (
-            'id' => '12345',
-            'tr_id' => 'TR-BRA-79FR0X',
-            'tr_date' => '2019-07-22 08:45:23',
-            'tr_crc' => 'order_200',
-            'tr_amount' => '40.96',
-            'tr_paid' => '40.96',
-            'tr_desc' => 'Tpay shop',
-            'tr_status' => 'TRUE',
-            'tr_error' => 'none',
-            'tr_email' => 'customer@example.com',
-            'test_mode' => '1',
-            'md5sum' => '0d1cf3083e2fe3b49d046c28e28d120c',
-        );
         Log::critical($request);
         try {
             $notificationApi = new TransactionNotificationApi();
