05012135ed8f7e003a121df2344812b3af78174f
Aleksander Szut <aleksander.szut@paymento.pl>
2021-01-18 23:30:41 +0100

    - debug

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -47,6 +47,8 @@ class TransactionController extends Controller
                         foreach ($payment->order->cart as $cart) {
                             $confession = new Confession();
                             $confession->package()->associate($cart->package);
+                            Log::warning(json_encode($cart->package));
+
                             $confession->uuid = Str::uuid();
                             $confession->title = null;
                             $confession->content = null;
